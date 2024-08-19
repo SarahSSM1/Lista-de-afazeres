ec9adf2d2a495fa147ec8b33b435eb3c5d106d28
SarahSSM1 <sarahsz204@gmail.com>
2024-08-19 17:03:15 -0300

    modificando servico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { ToDoComponent } from './components/to-do/to-do.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToDoComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/service/to-do.service.ts
+++ src/app/components/service/to-do.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToDo } from '../interface/toDo';
+import { catchError, Observable, retry, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,42 @@ export class ToDoService {
 
   constructor(private http: HttpClient) {}
 
-  getToDo(){
+  getLista(){
     return this.http.get<ToDo[]>(`${this.URL}`)
+      .pipe(
+      retry(2),
+      catchError(this.handleError))
+    }
+
+  getToDoId(id: number): Observable<ToDo[]>{
+    return this.http.get<ToDo[]>(this.URL + '/' + id)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  saveToDo(lista: ToDo): Observable<ToDo[]>{
+    return this.http.post<ToDo[]>(this.URL, JSON.stringify)
+      .pipe(
+        retry(2),
+        catchError(this.handleError))
+  }
+
+  deleteToDo(lista: ToDo): Observable<ToDo[]>{
+    return this.http.delete<ToDo[]>(this.URL + '/' + lista.id)
+      .pipe(
+        retry(1),
+        catchError(this.handleError))
+  }
+
+ private handleError(erro: HttpErrorResponse){
+  let errorMessage = '';
+    if (erro.error instanceof ErrorEvent) {
+      errorMessage = `Erro do lado do cliente: ${erro.error.message}`;
+    } else {
+      errorMessage = `Erro do lado do servidor: ${erro.message}`;
+    }
+
+  return throwError(() => new Error(errorMessage))
   }
 }
