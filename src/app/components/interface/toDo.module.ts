5e38fc3cd1ef9366b637f497443a42dae2c7a90c
SarahSSM1 <sarahsz204@gmail.com>
2024-08-19 09:48:05 -0300

    subindo alteracao no module

---
+++ src/app/components/interface/toDo.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToDoComponent } from '../to-do/to-do.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ToDoComponent]
+})
+export class ToDoModule { }
